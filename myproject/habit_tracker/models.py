7790ec2d44b5cf71f880d28aa6137fe0bf3171d4
AdamPrzychodniStudent <adam.przychodni@iu-study.org>
2023-07-29 13:56:12 +0200

    Fundamentals

--- myproject/habit_tracker/models.py
+++ myproject/habit_tracker/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Habit(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=200)
+    period = models.CharField(max_length=50)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+class CheckOff(models.Model):
+    habit = models.ForeignKey(Habit, on_delete=models.CASCADE)
+    timestamp = models.DateTimeField(auto_now_add=True)
--- myproject/habit_tracker/views.py
+++ myproject/habit_tracker/views.py
@@ -1,3 +1,30 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.contrib.auth.decorators import login_required
+from .models import Habit
+from .forms import HabitForm  # Import HabitForm here
 
-# Create your views here.
+@login_required
+def create_habit(request):
+    if request.method == "POST":
+        form = HabitForm(request.POST)
+        if form.is_valid():
+            habit = form.save(commit=False)
+            habit.user = request.user
+            habit.save()
+            return HttpResponseRedirect('/habits')
+    else:
+        form = HabitForm()
+
+    return render(request, 'habit_tracker/create_habit.html', {'form': form})
+
+
+from django.shortcuts import render
+from .models import Habit
+
+def habit_list(request):
+    habits = Habit.objects.all()
+    return render(request, 'habit_tracker/habit_list.html', {'habits': habits})
+
+
+
