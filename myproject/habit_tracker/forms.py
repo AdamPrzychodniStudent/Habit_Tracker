2b513ff6f35c6bba676ba479565f468d02e6601a
Adam Przychodni Student <adam.przychodni@iu-study.org>
2023-10-24 13:21:35 +0000

    tests

--- myproject/habit_tracker/forms.py
+++ myproject/habit_tracker/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from django.core.exceptions import ValidationError
 from .models import Habit
 
 # Custom DateInput widget for date fields
@@ -16,3 +17,13 @@ class HabitForm(forms.ModelForm):
             'start_date': DateInput(),
             'end_date': DateInput(),
         }
+
+    def clean(self):
+        cleaned_data = super().clean()
+        start_date = cleaned_data.get('start_date')
+        end_date = cleaned_data.get('end_date')
+
+        if start_date and end_date:
+            if start_date > end_date:
+                self.add_error('end_date', 'End date should be after start date.')
+                raise ValidationError('End date should be after start date.')
--- myproject/habit_tracker/test_habit_tracker.py
+++ myproject/habit_tracker/test_habit_tracker.py
@@ -1,10 +1,23 @@
 from django.test import TestCase
-
-# Create your tests here.
 import pytest
 from django.contrib.auth.models import User
 from django.utils import timezone
-from habit_tracker.models import Habit, CheckOff  # Adjust the import based on your project structure
+from habit_tracker.models import Habit, CheckOff  
+
+from django.test import TestCase
+from django.contrib.auth.models import User
+from django.core.management import call_command
+
+from datetime import datetime, date  
+
+import pytest
+from django.contrib.auth.models import User
+from django.utils import timezone
+from habit_tracker.models import Habit, CheckOff
+from habit_tracker.utils import update_timestamp, create_example_habits
+
+
+# Test for models.py
 
 @pytest.mark.django_db
 def test_create_habit():
@@ -40,3 +53,128 @@ def test_create_checkoff():
     
     assert CheckOff.objects.count() == 1
     assert checkoff.habit == habit
+
+# Tests for signals.py 
+
+class SignalTests(TestCase):
+
+    def test_create_initial_users(self):
+        # Delete all existing users to simulate a fresh database
+        User.objects.all().delete()
+
+        # Ensure there are no users before running migrations
+        self.assertEqual(User.objects.count(), 0)
+        
+        # Run migrations, which should trigger the post_migrate signal
+        call_command('migrate')
+        
+        # Test that the users have been created
+        self.assertEqual(User.objects.count(), 2)
+        
+        # Test that a superuser has been created
+        self.assertTrue(User.objects.filter(is_superuser=True).exists())
+        
+        # Test that a regular user has been created
+        self.assertTrue(User.objects.filter(username='user').exists())
+
+
+
+# Tests for utils.py 
+
+@pytest.mark.django_db
+def test_create_example_habits():
+    user = User.objects.create_user(username='john', password='1234')
+
+    # There should be no habits initially
+    assert Habit.objects.count() == 0
+
+    create_example_habits(user)
+
+    # Debug: Print all habit names to the console
+    for habit in Habit.objects.all():
+        print(habit.name)
+
+    # Now there should be 7 habits created for the user
+    assert Habit.objects.count() == 7
+
+
+@pytest.mark.django_db
+def test_create_example_habits():
+    user = User.objects.create_user(username='john', password='1234')
+
+    # There should be no habits initially
+    assert Habit.objects.count() == 0
+
+    create_example_habits(user)
+
+    # Now there should be 6 habits created for the user
+    assert Habit.objects.count() == 6
+
+    # Check if the habits have the right number of check-offs
+    exercise_habit = Habit.objects.get(name="Exercise (Time Ended)")
+    assert CheckOff.objects.filter(habit=exercise_habit).count() == 7
+
+    habit2 = Habit.objects.get(name="Read Every Day (Fully Completed)")
+    assert CheckOff.objects.filter(habit=habit2).count() == 30
+
+    habit_long_streak = Habit.objects.get(name="Meditate (Long Streak)")
+    assert CheckOff.objects.filter(habit=habit_long_streak).count() == 365
+
+    habit_short_streak = Habit.objects.get(name="Learn Something New (Short Streak)")
+    assert CheckOff.objects.filter(habit=habit_short_streak).count() == 4
+
+    kebab_habit = Habit.objects.get(name="Eat Kebab :) (Weekly)")
+    assert CheckOff.objects.filter(habit=kebab_habit).count() == 3
+
+    connect_habit = Habit.objects.get(name="Connect with Loved Ones (Monthly)")
+    assert CheckOff.objects.filter(habit=connect_habit).count() == 3
+
+# Test for forms.py 
+
+from django.test import TestCase
+from habit_tracker.forms import HabitForm
+
+# Test for forms.py
+
+class HabitFormTest(TestCase):
+
+    def test_valid_form(self):
+        data = {
+            'name': 'Test Habit',
+            'period': 'Daily',
+            'start_date': '2022-01-01',
+            'end_date': '2022-12-31'
+        }
+        form = HabitForm(data)
+        self.assertTrue(form.is_valid())
+
+    def test_invalid_form(self):
+        data = {
+            'name': '',  # name is empty
+            'period': 'Daily',
+            'start_date': '2022-01-01',
+            'end_date': '2022-12-31'
+        }
+        form = HabitForm(data)
+        self.assertFalse(form.is_valid())
+        
+    def test_required_fields(self):
+        data = {
+            # Missing 'name' and 'period'
+            'start_date': '2022-01-01',
+            'end_date': '2022-12-31'
+        }
+        form = HabitForm(data)
+        self.assertFalse(form.is_valid())
+        self.assertEqual(set(form.errors.keys()), {'name', 'period'})
+
+    def test_date_constraints(self):
+        data = {
+            'name': 'Test Habit',
+            'period': 'Daily',
+            'start_date': '2022-12-31',  # start_date > end_date
+            'end_date': '2022-01-01'
+        }
+        form = HabitForm(data)
+        self.assertFalse(form.is_valid())
+        self.assertIn('end_date', form.errors)
